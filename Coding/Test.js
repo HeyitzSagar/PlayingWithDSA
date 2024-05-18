c82efb22ac42ba718c94418942d794809ef3cb58
Sagar Gupta <sagargupta.gola@gmail.com>
2024-05-18 13:25:40 +0530

    crudcrook

---
+++ Coding/Test.js
@@ -0,0 +1,49 @@
+// let array = [-1, -3, 6, 2, -3]
+
+// let sortedarray = array.sort((a, b) => b-a);
+// let maxmthreedigit = sortedarray.slice(0, 3);
+// let product = -1;
+// for (let i = 0; i < maxmthreedigit.length; i++) {
+//     product = product * maxmthreedigit[i];
+
+// }
+// console.log(product)
+
+
+// let array = [-1, -2, -3, -6, -7]
+
+// let output = 
+
+
+// // output = 18;
+
+// // function maximumProduct(array)
+// // {
+// //     let sortedarray = array.sort((a,b) => b-a);
+// //     let maxmthreedigit = sortedarray.slice(0, 3);
+// //     let product = 1;
+// //     for (let i = 0; i < maxmthreedigit.length; i++) {
+// //             product = product +  product * maxmthreedigit[i];
+// //     }
+// //     return product;
+// // }
+
+// // console.log(maximumProduct(array))
+// // let array = [10, 20, 30, 40];
+
+// // let newarr = array.slice(0, 3);
+// // console.log(newarr)
+
+
+
+
+let array = [1, 2, 5, 4, 3];   // yes   [5, 4, 3] --> [3, 4, 5]
+let array2 = [1, 2, 3, 5, 4, 6]; //  yes
+let array3 = [1, 2, 5, 3, 4];  // false
+
+
+
+
+
+
+
